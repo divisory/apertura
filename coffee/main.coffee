bf0f8b05c0215928bbb6e713297550c6bf7fbbc3
divisory <kandyor2303@gmail.com>
2015-06-09 01:31:57 +0300

    release v1.0

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -2,8 +2,24 @@ $ document
 
 	.ready ->
 
-		#
-		# your code here
-		# 
+		handle = (hash)->
+			to = $(hash).position().top - 60
+			$ 'html,body'
+				.animate {scrollTop: to}, 1000
+			location.hash = hash
+
+		$ '.anchors li a'
+			.click (e)->
+				e.preventDefault()
+				handle "#{$(this).attr('href')}"
+
+		if location.hash.length > 0
+			handle location.hash
+
+		$ '.show-menu'
+			.click (e)->
+				e.preventDefault()
+				$(this).toggleClass 'active'
+				$('.main-nav').toggleClass 'active'
 
 		return
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,26 @@
 (function() {
-  $(document).ready(function() {});
+  $(document).ready(function() {
+    var handle;
+    handle = function(hash) {
+      var to;
+      to = $(hash).position().top - 60;
+      $('html,body').animate({
+        scrollTop: to
+      }, 1000);
+      return location.hash = hash;
+    };
+    $('.anchors li a').click(function(e) {
+      e.preventDefault();
+      return handle("" + ($(this).attr('href')));
+    });
+    if (location.hash.length > 0) {
+      handle(location.hash);
+    }
+    $('.show-menu').click(function(e) {
+      e.preventDefault();
+      $(this).toggleClass('active');
+      return $('.main-nav').toggleClass('active');
+    });
+  });
 
 }).call(this);
